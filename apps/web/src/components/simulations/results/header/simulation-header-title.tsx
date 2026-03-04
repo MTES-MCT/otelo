455ae467ad1073063c30b6f84c0628dec6f2233c
Kevin GALLET <kevingallet@gmail.com>
2026-03-04 17:42:03 +0100

    fix: last millesime

--- apps/web/src/components/simulations/results/header/simulation-header-title.tsx
+++ apps/web/src/components/simulations/results/header/simulation-header-title.tsx
@@ -1,11 +1,11 @@
 import Tag from '@codegouvfr/react-dsfr/Tag'
 
-export const SimulationHeaderTitle = ({ name, projection }: { name: string; projection: number }) => {
+export const SimulationHeaderTitle = ({ name, projection, millesime }: { name: string; projection: number; millesime: string }) => {
   return (
     <div className="fr-flex fr-align-items-center fr-flex-gap-4v">
       <h1 className="fr-h2 fr-text-title--blue-france fr-mb-0">{name}</h1>
       <Tag as="span" nativeSpanProps={{ className: 'fr-background-default--grey fr-text-title--blue-france fr-text--medium' }}>
-        2021 → {projection}
+        {millesime} → {projection}
       </Tag>
     </div>
   )
