be2c21dcddfcd70f1d644e6ebd2bcfe775ff69b4
Kevin GALLET <kevingallet@gmail.com>
2026-03-19 08:34:56 +0100

    feat: export template alexandre

--- packages/shared/src/schemas/statistics/template-statistics.ts
+++ packages/shared/src/schemas/statistics/template-statistics.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
 
 export const ZTemplateStatisticsRow = z.object({
-  Prénom: z.string(),
-  Nom: z.string(),
-  Email: z.string(),
+  'Prénom': z.string(),
+  'Nom': z.string(),
+  'Email': z.string(),
   'Date de création compte Otelo': z.string(),
   'Date derniere estimation': z.string().nullable(),
   'EPCIs Code estimés': z.string(),
